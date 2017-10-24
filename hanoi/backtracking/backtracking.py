b06dff7ab08179958dfda6c50f976f39b7ed213a
robertbutacu <robert.butacu@yahoo.com>
2017-10-24 17:37:27 +0300

    hello world

--- hanoi/backtracking/backtracking.py
+++ hanoi/backtracking/backtracking.py
@@ -1,21 +1,25 @@
 from hanoi.utils.hanoi import *
+import time
 
 
 def start_backtracking(pegs, pieces):
-    def go(state, current_piece_index):
+    def go(state, current_piece_index, previous_states):
         if is_final_state(state):
-            return state
+            print("Found state! " + str(state))
 
-        for current_peg in range(1, pegs + 1):
-            if is_valid(state, current_piece_index, current_peg):
-                new_state = transition(state, current_piece_index, current_peg)
-                print(str(new_state) + "\n")
-                for next_piece in range(1, state[0] + 1):
-                    if next_piece != current_piece_index:
-                        go(new_state, next_piece)
+        for peg in range(1, state[0] + 1):
+            if is_valid(state, current_piece_index, peg):
+                new_state = transition(state, current_piece_index, peg)
+                print("Current state " + str(new_state))
+                print("Current piece index " + str(current_piece_index))
+                print("Previous states " + str(previous_states))
+                time.sleep(2)
+                for x in range(1, new_state[0] + 1):
+                    if x != current_piece_index:
+                        return go(new_state, x, previous_states +  [new_state])
 
     initial_state = initialize(pegs, pieces)
-    return go(initial_state, 1)
+    return go(initial_state, 1, [])
 
 
 print(start_backtracking(3, 3))
