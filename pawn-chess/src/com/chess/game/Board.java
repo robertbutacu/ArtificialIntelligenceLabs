92baefa4502586e604350274a0d12fb866a40618
robertbutacu <robert.butacu@yahoo.com>
2017-10-28 20:16:39 +0300

    added move

--- pawn-chess/src/com/chess/game/Board.java
+++ pawn-chess/src/com/chess/game/Board.java
@@ -1,5 +1,7 @@
 package com.chess.game;
 
+import javafx.util.Pair;
+
 public class Board {
     private int [][] board;
 
@@ -8,6 +10,20 @@ public class Board {
         initiateBoard();
     }
 
+    public void move(int player, Pair<Integer, Integer> from, Pair<Integer, Integer> to){
+        if(isValidPlayerTurn(player) && isPlayerPiece(player, from)){
+            board[from.getKey()][from.getValue()] = 0;
+            board[to.getKey()][to.getValue()] = player;
+        }
+    }
+
+    private Boolean isValidPlayerTurn(int player){
+        return player == 1 || player == 2;
+    }
+
+    private Boolean isPlayerPiece(int player, Pair<Integer, Integer> coordinates){
+        return board[coordinates.getKey()][coordinates.getValue()] == player;
+    }
 
     private void initiateBoard() {
         for (int i = 0; i < 8; i++) {
