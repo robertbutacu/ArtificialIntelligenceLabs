5c983dd521f07964a9b516d80a2c1469973eec82
robertbutacu <robert.butacu@yahoo.com>
2017-10-28 20:01:28 +0300

    initialization toString

--- pawn-chess/src/com/chess/Main.java
+++ pawn-chess/src/com/chess/Main.java
@@ -1,9 +1,13 @@
 package com.chess;
 
+import com.chess.game.Board;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        Board board = new Board();
+
+        System.out.println(board);
     }
 }
---
+++ pawn-chess/src/com/chess/game/Board.java
@@ -0,0 +1,43 @@
+package com.chess.game;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Board {
+    private int [][] board;
+
+    public Board() {
+        board = new int[8][8];
+        initiateBoard();
+    }
+
+
+    private void initiateBoard() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if(i == 1)
+                    board[i][j] = 1;
+                else if(i == 6)
+                    board[i][j] = 2;
+                else
+                    board[i][j] = 0;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder boardView = new StringBuilder();
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                boardView.append(board[i][j]);
+                boardView.append(" ");
+            }
+
+            boardView.append("\n");
+        }
+
+        return boardView.toString();
+    }
+}
