34b2f1bd81356e16f091cfe726c206e22b222269
robertbutacu <robert.butacu@yahoo.com>
2017-11-01 19:58:19 +0200

    initializeBoard() renaming

--- pawn-chess/src/com/chess/game/Board.java
+++ pawn-chess/src/com/chess/game/Board.java
@@ -11,7 +11,7 @@ public class Board {
 
     Board() {
         board = new int[8][8];
-        initiateBoard();
+        initializeBoard();
     }
 
     /**
@@ -149,7 +149,7 @@ public class Board {
     }
 
 
-    private void initiateBoard() {
+    private void initializeBoard() {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (i == 1)
