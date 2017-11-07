c9092ed298974f7e505cc400abea45c601299805
robertbutacu <robert.butacu@yahoo.com>
2017-11-07 19:21:10 +0200

    all goochy for definitions

--- text-parser/src/main/scala/parser/Parser.scala
+++ text-parser/src/main/scala/parser/Parser.scala
@@ -10,24 +10,36 @@ object Parser {
             definitionFilename: String,
             textFilename: String,
             outputFilename: String): Unit = {
-    try{
-      val contextFile    = Source.fromFile(contextFilename).getLines().toList
+    try {
+      val contextFile = Source.fromFile(contextFilename).getLines().toList
       val definitionFile = Source.fromFile(definitionFilename).getLines().toList
-      val textFile       = Source.fromFile(textFilename).getLines().toList
-      val outputFile     = Source.fromFile(outputFilename)
+      val textFile = Source.fromFile(textFilename).getLines().toList
+      val outputFile = Source.fromFile(outputFilename)
 
       val splitContext = contextFile.map(_.split("\\s+=\\s+"))
-      val splitDefinitions = definitionFile.flatMap(_.split("\\s+=\\s+"))
-      val splitText = textFile.flatMap(_.split("\\s+=\\s+"))
+      val splitDefinitions = definitionFile.map(_.split("\\s+=\\s+"))
+      val splitText = textFile.map(_.split("\\s+=\\s+"))
 
-      val split = splitContext.map(_.flatMap(_.split(";\\s+")).toList)
+      var split = splitContext.map(_.flatMap(_.split(";\\s+")).toList)
 
       val context = split.map(e => Context(e.head, e.tail))
 
-      println(context)
-    } catch{
+      split = splitDefinitions.map(_.flatMap(_.split("\\s+=\\s+")).toList)
+
+      val definitionsNotNormalized = split.map(_.head).zip(split.map(e => e.tail.flatMap(b => b.split(";\\s+"))))
+
+      val definitions = definitionsNotNormalized.map(d => Definition(d._1,
+        d._2.map(t => (getContext(t),getDefinition(t))).toMap))
+
+      println(definitions)
+
+    } catch {
       case _: FileNotFoundException => println("Invalid file name!")
-      case _: IOException           => println("Oups! Its not you, its us!")
+      case _: IOException => println("Oups! Its not you, its us!")
     }
   }
+
+  private def getContext(t: String): String = t.span(_ != ']')._1.dropWhile(c => c == ' ' || c == '[')
+
+  private def getDefinition(t: String): String =t.span(_ != ']')._2.dropWhile(c => c == ' ' || c == ']')
 }
